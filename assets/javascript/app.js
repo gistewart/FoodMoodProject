6bb643c8af333be76cf9c3fe3205bd735cd48d11
Farrah Carter <carterfarraha@gmail.com>
2019-10-30 13:00:56 -0400

    gifs are retrieved and are appended to the dom

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,34 @@
 function getGif() {
-    //takes animal name from input entered
-    let foodInput = "spaghetti";
+    //variable for input
+    let foodInput = "donut";
 
-    //resets page count if different animal chosen
-    if(currentAnimal != animalName){
-        gifPage = 0;
-    }
 
     //object containing parameters 
     const queryParams = {
         "api_key": "CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2",
-        q: animalName,
-        "limit": 10,
-        "offset": gifPage,
+        q: foodInput,
+        "limit": 1,
+        "offset": 0,
         "rating": "G",
         "lang": "en"
     };
+
     //call parameters from object
     let paramString = $.param(queryParams);
-    let queryURL = "https://api.giphy.com/v1/gifs/search?" + paramString;
-    //queryURL = "https://api.giphy.com/v1/gifs/search?api_key=CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2&q=" + animalName + "&limit=10&offset=0&rating=G&lang=en"
+    let gifQueryURL = "https://api.giphy.com/v1/gifs/search?" + paramString;
 
     $.ajax({
         //calls giphy search
-        url: queryURL,
+        url: gifQueryURL,
         Method: "GET"
     }).then(function (response) {
         console.log(response);
-        //creates image div for each item in response
-        for (let i = 0; i < response.data.length; i++) {
-            const gifContent = `<div class="col-2.4">
-            ${"<img src=" + response.data[i].images.fixed_width_still.url + "/>"}</div>`
-            $("#gifRow").prepend(gifContent);
-
-        }
-
-        //loads different gifs on next click
-        gifPage += 10;
-
-        //sets current animal
-        currentAnimal = animalName;
-
+        //creates image div and appends to DOM
+        const gifContent = "<img src=" + response.data[0].images.fixed_width.url + "/>";
+        $("#gifHolder").append(gifContent);
     })
 }
+
+$(document).ready(function () {
+    getGif();
+})
