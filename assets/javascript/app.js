aff4a1781b7267c033fb468a257ecbec6d84eb0a
Farrah Carter <carterfarraha@gmail.com>
2019-10-31 11:58:49 -0400

    added refresh buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,13 +1,16 @@
 //variable for input
 let foodInput = "spaghetti";
+let gifOffset = 0;
+let imgPage = 1;
 
 function getGif() {
+    $("#gifDiv").empty()
     //object containing parameters 
     const gifQueryParams = {
         "api_key": "CbRv29mIUSwkTAVauYUvcQ8lOGyxCop2",
         q: foodInput,
         "limit": 1,
-        "offset": 0,
+        "offset": gifOffset,
         "rating": "G",
         "lang": "en"
     };
@@ -25,15 +28,21 @@ function getGif() {
         //creates image div and appends to DOM
         const gifContent = "<img src=" + response.data[0].images.fixed_width.url + "/>";
         $("#gifDiv").append(gifContent);
+
+        //creates refresh button
+        const refreshGifBtn = "<p class='refresh' id='refreshGif'>&#8635;</p>";
+        $("#gifDiv").prepend(refreshGifBtn);
     })
 }
 
 function getPic() {
 
+    $("#imgDiv").empty()
+
     const imgQueryParams = {
         query: foodInput,
         "per_page": 3,
-        "page": 1
+        "page": imgPage
     }
 
     let imgParamString = $.param(imgQueryParams);
@@ -49,9 +58,13 @@ function getPic() {
     }).then(function (response) {
         console.log(response);
         for (let i = 0; i < response.photos.length; i++) {
+            //adds image to the DOM
             let imgContent = "<img src=" + response.photos[i].src.tiny + "/>";
             $("#imgDiv").append(imgContent);
         }
+        //creates refresh button
+        const refreshImgBtn = "<p class='refresh' id='refreshImg'>&#8635;</p>";
+        $("#imgDiv").prepend(refreshImgBtn);
     })
 
 }
@@ -59,4 +72,21 @@ function getPic() {
 $(document).ready(function () {
     getGif();
     getPic();
+
+    //refresh gif function
+    $(document).on("click", "#refreshGif", function (){
+        gifOffset++;
+        getGif();
+
+    })
+
+    //refresh images function
+    $(document).on("click", "#refreshImg", function (){
+        imgPage++;
+        console.log(imgPage);
+        getPic();
+
+    })
+
+
 })
