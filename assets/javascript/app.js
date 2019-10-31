703b119abcdfa6f315e7c3e1eb485f0bf4a21e65
Jonathan Johnson <jraf7@icloud.com>
2019-10-31 12:28:40 -0400

    remove recipe.js & compile to existing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -69,6 +69,37 @@ function getPic() {
 
 }
 
+function recipe() {
+    //variable for input
+    let foodrecipe = "donut";
+  
+    //object containing parameters
+    const queryPara = {
+      key: "7982d935e15cd8e88f053be3be874c94",
+      q: foodrecipe
+    };
+  
+    //call parameters from object
+    let paraString = $.param(queryPara);
+    let recipeQueryURL = "https://www.food2fork.com/api/search?" + paraString;
+  
+    $.ajax({
+      //calls giphy search
+      url: recipeQueryURL,
+      Method: "GET"
+    }).then(function(response) {
+      response = JSON.parse(response);
+  
+      console.log(response);
+  
+      console.log("Recipe: " + response.recipes[1].source_url);
+      console.log("Title: " + response.recipes[1].title);
+    });
+  }
+  
+  recipe();
+  
+
 $(document).ready(function () {
     getGif();
     getPic();
@@ -90,3 +121,4 @@ $(document).ready(function () {
 
 
 })
+
